13a9e355491f2c1e51de6fb0fbc8dd6cad5a8f17
JungwooSim <cscd053@gmail.com>
2026-01-03 21:33:48 +0900

    16. 쿠버네티스에 마이크로서비스 배포

--- spring-cloud/config-server/src/main/java/se/magnus/springcloud/configserver/SecurityConfig.java
+++ spring-cloud/config-server/src/main/java/se/magnus/springcloud/configserver/SecurityConfig.java
@@ -13,8 +13,10 @@ public class SecurityConfig {
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
     // Disable CSRF to allow POST to /encrypt and /decrypt endpoints
     http.csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
-        .httpBasic(Customizer.withDefaults());
+     .authorizeHttpRequests(requests -> requests
+      .requestMatchers("/actuator/**").permitAll()
+      .anyRequest().authenticated())
+     .httpBasic(Customizer.withDefaults());
     return http.build();
   }
 }
